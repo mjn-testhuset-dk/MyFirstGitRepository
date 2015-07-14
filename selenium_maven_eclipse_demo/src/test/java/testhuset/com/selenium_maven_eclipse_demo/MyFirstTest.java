6d5f94c206c26bc75ed420e2ac7b792766030307
Michael Juel Nielsen <mjn@testhuset.dk>
2015-07-14 12:52:10 +0200

    Første CheckIn af Demo projekt

---
+++ selenium_maven_eclipse_demo/src/test/java/testhuset/com/selenium_maven_eclipse_demo/MyFirstTest.java
@@ -0,0 +1,21 @@
+package testhuset.com.selenium_maven_eclipse_demo;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import junit.framework.Assert;
+
+
+public class MyFirstTest {
+	
+	@Test
+	public void startWebDriver(){
+		WebDriver driver = new FirefoxDriver();
+		driver.navigate().to("http://seleniumsimplified.com");	
+		Assert.assertTrue("Title should start differently", 
+				driver.getTitle().startsWith("Selenium Simplified"));
+		driver.close();
+	}
+
+}
